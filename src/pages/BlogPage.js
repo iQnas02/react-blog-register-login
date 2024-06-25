2adeb4bee5fe7e8f2f761e70392e4f4b982f9d84
unknown <ignasurbonas02@gmail.com>
2024-06-25 15:13:20 +0300

    pagination added

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,15 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import http from "../plugins/http";
 import SinglePost from "../components/SinglePost";
+import Pagination from "../pages/Pagination";
 
 const BlogPage = ({loggedIn}) => {
 
-    const [data,setData] = useState([])
+    const [data,setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage]= useState(24);
 
     function getPosts() {
         http.get("/getallposts")
             .then(res =>{
-                // console.log(res)
                 setData(res.data.reverse())
             })
     }
@@ -18,9 +20,26 @@ const BlogPage = ({loggedIn}) => {
         getPosts()
     }, [])
 
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+
     return (
-        <div className="d-flex flex-wrap">
-            {data.map(x => <SinglePost getPosts={getPosts} loggedIn={loggedIn} key={x.id} post={x}/>)}
+        <div className="d-flex flex-column align-items-center">
+            <div className="d-flex flex-wrap">
+                {currentPosts.map(post => (
+                    <SinglePost getPosts={getPosts} loggedIn={loggedIn} key={post.id} post={post}/>
+                ))}
+            </div>
+            <Pagination
+                postsPerPage={postsPerPage}
+                totalPosts={data.length}
+                paginate={paginate}
+                currentPage={currentPage}
+            />
         </div>
     );
 };
